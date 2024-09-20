2b4b1fd20bb84b666fdc80bd043f9b2a245e1c74
Glory Alphonsus <81162281+Kupenya@users.noreply.github.com>
2024-09-20 07:22:52 +0100

    .

--- server.js
+++ server.js
@@ -33,8 +33,14 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
+const corsOptions = {
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
 const app = express();
-app.use(cors());
+app.use(cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
